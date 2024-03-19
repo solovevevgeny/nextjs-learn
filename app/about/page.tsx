9d0f2d72b32397107a088bd92f16b858b35b2180
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-03-19 12:13:05 +0300

    add metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'About us',
+    'description': 'Page about us'
+}
+
 export default function About() {
     return <h3>About page</h3>
 }
